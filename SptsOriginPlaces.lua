667734aaf36cc347db3bcaac3cdff09c53e75256
Lost <93594637+ItzLost@users.noreply.github.com>
2023-02-25 17:49:05 +0100

    Create SptsOriginPlaces.lua

---
+++ SptsOriginPlaces.lua
@@ -0,0 +1,14 @@
+local Places = {
+  Fist = {
+    [1] = CFrame.new(-352, 15725, 16),
+    [2] = CFrame.new(1380, 9274, 1647),
+    [3] = CFrame.new(1177, 4789, -2294),
+  },
+  
+  Psy = {
+    [1] = CFrame.new(-2558, 5416, -507), -- Flying Island Waterfall 1Qa
+    [2] = CFrame.new(-2582, 5516, -505), -- Flying Island Bridge 1T
+    [3] = CFrame.new(-2563, 5501, -434), -- Flying Island 1B
+    [4] = CFrame.new(-2532, 5486, -528), -- Flying Island 1M
+  },
+}
